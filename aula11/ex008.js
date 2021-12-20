69a0f4fd4751ec2a0497a325a688d7110136fa6d
Tiago Pereira Barreto <93627992+TiagoPBarreto@users.noreply.github.com>
2021-12-19 21:39:07 -0300

    aula de Js

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+let vel = 80
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if(vel > 60) {
+    console.log('Você utrapassou a velocidade maxima permitida. MULTADO')
+}
+console.log('Dirija sempre usando o cinto de segurança')
