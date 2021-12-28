1d49040f6772814e13e828f751d8dcf8f6f3f181
Tiago Pereira Barreto <93627992+TiagoPBarreto@users.noreply.github.com>
2021-12-28 20:59:43 -0300

    ex017

--- ex017/script.js
+++ ex017/script.js
@@ -1,22 +1,21 @@
-function carregar() {
-    let msg = window.document.getElementById('msg')
-    let img = window.document.getElementById('imagem')
-    let data = new Date() 
-    let hora = data.getHours()
-    msg.innerHTML = ` Agora são ${ hora} horas ` 
-    if(hora >= 0 && hora < 12) {
-        //BOM DIA
-        img.src = "fotomanha.png"
-        document.body.style.background = "#e2cd9f"
-    }  else if (hora >= 12 && hora < 18) {
-        //BOA TARDE
-        img.src = "fototarde.png"
-        document.body.style.background = "#b9846f"
-    } else{
-        //BOA NOITE
-        img.src = "fotonoite.png"
-        document.body.style.background = "#515154"
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while(c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} * ${c} = ${n*c}`
+            item.value = `tab ${c}`
+            tab.appendChild(item)
+            c++
+        }
     }
-}
+   
+}  
  
 
